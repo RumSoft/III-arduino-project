1cd7fa38688c022bcbfb2c34d21c422049a0b8b7
Dominik Ruman <dr124@o2.pl>
2019-01-27 13:15:18 +0100

    separate files even more

--- source/header.h
+++ source/header.h
@@ -15,3 +15,52 @@ inline float betweenF(float val, float a, float b)
 		return b;
 	return val;
 }
+
+//c == 0? if value > 0 then return value
+//c == 1? if value < 0 then return -value
+inline int separateValue(int val, int c)
+{
+	return min(4090, c == 0
+						 ? val > 0 ? val : 0
+						 : val < 0 ? -val : 0);
+}
+
+void writeOutput(int value)
+{
+	dacc_set_channel_selection(DACC_INTERFACE, 0);
+	dacc_write_conversion_data(DACC_INTERFACE, separateValue(value, 1));
+	dacc_set_channel_selection(DACC_INTERFACE, 1);
+	dacc_write_conversion_data(DACC_INTERFACE, separateValue(value, 0));
+}
+
+void my_setup() {
+    /* turn on the timer clock in the power management controller */
+	pmc_set_writeprotect(false);
+	pmc_enable_periph_clk(ID_TC4);
+
+	/* we want wavesel 01 with RC */
+	TC_Configure(/* clock */ TC1, /* channel */ 1, TC_CMR_WAVE | TC_CMR_WAVSEL_UP_RC | TC_CMR_TCCLKS_TIMER_CLOCK2);
+	TC_SetRC(TC1, 1, 238); // sets <> 44.1 Khz interrupt rate
+	//TC_SetRC(TC1, 1, 109); // sets <>   96 Khz interrupt rate
+
+	TC_Start(TC1, 1);
+
+	// enable timer interrupts on the timer
+	TC1->TC_CHANNEL[1].TC_IER = TC_IER_CPCS;
+	TC1->TC_CHANNEL[1].TC_IDR = ~TC_IER_CPCS;
+
+	/* Enable the interrupt in the nested vector interrupt controller */
+	/* TC4_IRQn where 4 is the timer number * timer channels (3) + the channel number
+	(=(1*3)+1) for timer1 channel1 */
+	NVIC_EnableIRQ(TC4_IRQn);
+
+	//ADC Configuration
+	ADC->ADC_MR |= 0x80;	// DAC in free running mode.
+	ADC->ADC_CR = 2;		// Starts ADC conversion.
+	ADC->ADC_CHER = 0x1CC0; // Enable ADC channels 0 and 1.
+
+	//enable DAC
+	analogWrite(DAC0, 0);
+	analogWrite(DAC1, 0);
+
+}
--- source/source.ino
+++ source/source.ino
@@ -27,53 +27,18 @@ float frac = 0;
 float Seno_table[no_samples];
 int n, j = 0;
 
-void cria_seno()
+void generate_sine()
 {
 	for (n = 0; n < no_samples; n++)
 		Seno_table[n] = (0.99 * cos(((2.0 * PI) / no_samples) * n));
 }
 
-//c == 0? if value > 0 then return value
-//c == 1? if value < 0 then return -value
-inline int separateValue(int val, int c)
-{
-	return min(4090, c == 0
-						 ? val > 0 ? val : 0
-						 : val < 0 ? -val : 0);
-}
+
 
 void setup()
 {
-	cria_seno();
-	/* turn on the timer clock in the power management controller */
-	pmc_set_writeprotect(false);
-	pmc_enable_periph_clk(ID_TC4);
-
-	/* we want wavesel 01 with RC */
-	TC_Configure(/* clock */ TC1, /* channel */ 1, TC_CMR_WAVE | TC_CMR_WAVSEL_UP_RC | TC_CMR_TCCLKS_TIMER_CLOCK2);
-	TC_SetRC(TC1, 1, 238); // sets <> 44.1 Khz interrupt rate
-	//TC_SetRC(TC1, 1, 109); // sets <>   96 Khz interrupt rate
-
-	TC_Start(TC1, 1);
-
-	// enable timer interrupts on the timer
-	TC1->TC_CHANNEL[1].TC_IER = TC_IER_CPCS;
-	TC1->TC_CHANNEL[1].TC_IDR = ~TC_IER_CPCS;
-
-	/* Enable the interrupt in the nested vector interrupt controller */
-	/* TC4_IRQn where 4 is the timer number * timer channels (3) + the channel number
-	(=(1*3)+1) for timer1 channel1 */
-	NVIC_EnableIRQ(TC4_IRQn);
-
-	//ADC Configuration
-	ADC->ADC_MR |= 0x80;	// DAC in free running mode.
-	ADC->ADC_CR = 2;		// Starts ADC conversion.
-	ADC->ADC_CHER = 0x1CC0; // Enable ADC channels 0 and 1.
-
-	//enable DAC
-	analogWrite(DAC0, 0);
-	analogWrite(DAC1, 0);
-
+	generate_sine();
+	my_setup();
 	pinMode(LED_BUILTIN, OUTPUT);
 }
 
@@ -91,6 +56,15 @@ void loop()
 	POT1 = ADC->ADC_CDR[11];
 	POT2 = ADC->ADC_CDR[12];
 	POT3 = ADC->ADC_CDR[13];
+}
+
+void updateValues()
+{
+	TC_GetStatus(TC1, 1); //DAC
+	value = in_ADC0 - in_ADC1;
+
+	selectedDelay = map(POT2 >> 5, 0, 128, 0, maxDelay);
+	selectedFreqShift = map(POT0 >> 5, 0, 128, 0, maxFreqShift);
 }
 
 int processFreqShift(int value, int shift)
@@ -138,27 +112,13 @@ int processFlanger(int value)
 	return (DELAY[delay_int + 2] * frac + DELAY[delay_int] * (1 - frac));
 }
 
-void updatePots()
-{
-	selectedDelay = map(POT2 >> 5, 0, 128, 0, maxDelay);
-	selectedFreqShift = map(POT0 >> 5, 0, 128, 0, maxFreqShift);
-}
 
-void writeOutput(int value)
-{
-	dacc_set_channel_selection(DACC_INTERFACE, 0);
-	dacc_write_conversion_data(DACC_INTERFACE, separateValue(value, 1));
-	dacc_set_channel_selection(DACC_INTERFACE, 1);
-	dacc_write_conversion_data(DACC_INTERFACE, separateValue(value, 0));
-}
 
 void TC4_Handler()
 {
-	// We need to get the status to clear it and allow the interrupt to fire again
-	TC_GetStatus(TC1, 1);
-	value = in_ADC0 - in_ADC1;
+	
 
-	updatePots();
+	updateValues();
 
 	value = processFlanger(value);
 	//value = processFreqShift(value, selectedFreqShift);
@@ -166,3 +126,4 @@ void TC4_Handler()
 
 	writeOutput(value);
 }
+
