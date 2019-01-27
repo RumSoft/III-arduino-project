d5bd85767cbec8944d4aaabaef29351b6bc92dc9
Dominik Ruman <dr124@o2.pl>
2019-01-27 16:00:13 +0100

    x

--- source/effects.h
+++ source/effects.h
@@ -5,12 +5,6 @@ int freqShiftArray[maxFreqShift];
 unsigned int selectedFreqShift = 0;
 unsigned int freqShiftCounter = 0;
 
-//delay
-#define maxDelay 15000
-int delayArray[maxDelay];
-unsigned int selectedDelay = 0;
-unsigned int delayCounter = 0;
-
 int processFreqShift(int value, int _shift)
 {
 	if(_shift < maxFreqShift / 100)
@@ -21,6 +15,13 @@ int processFreqShift(int value, int _shift)
 	return (freqShiftArray[freqShiftCounter] + value)>>1; 
 }
 
+
+//delay
+#define maxDelay 15000
+int delayArray[maxDelay];
+unsigned int selectedDelay = 0;
+unsigned int delayCounter = 0;
+
 int processDelay(int value, int _delay)
 {
 	if(_delay < maxDelay/100)
@@ -29,4 +30,45 @@ int processDelay(int value, int _delay)
 	delayArray[delayCounter] = (value + delayArray[delayCounter]) >> 1;
 	delayCounter = (delayCounter + 1) % _delay;
 	return delayArray[delayCounter];
+}
+
+
+//chorus
+#define maxChorusSpeed 30
+#define maxChorusDepth 80
+unsigned int selectedChorusSpeed = 0;
+unsigned int selectedChorusDepth = 0;
+
+#define MAX_DELAY 80
+#define chorusOffset 2
+int DELAY[MAX_DELAY + chorusOffset];
+int Delay2 = 0;
+float delay_sr = 0;
+int delay_int = 0;
+float frac = 0;
+int j = 0;
+
+int processChorus(int value)
+{
+	POT0 = map(POT0 >> 2, 0, 1024, 1, 60); //empirical adjusts
+	Delay2 = POT0 / 2;
+
+	for (int u = 0; u <= POT0; u++)
+		DELAY[POT0 + 1 - u] = DELAY[POT0 - u];
+
+	DELAY[0] = value;
+
+	POT1 = map(POT1 >> 2, 0, 1024, 1, 16); // empirical adjusts
+
+	delay_sr = Delay2 - Delay2 * sinf_8k[(j * POT1) / 20];
+	delay_int = int(delay_sr);
+	frac = delay_sr - delay_int;
+
+	frac = betweenF(frac, 0.01, 0.99);
+
+	j++;
+	if (j * POT1 / 20 >= sine_samples)
+		j = 0;
+
+	return (DELAY[delay_int + chorusOffset] * frac + DELAY[delay_int] * (1 - frac));
 }
--- source/source.ino
+++ source/source.ino
@@ -7,23 +7,6 @@ int out_DAC0, out_DAC1;
 int POT0, POT1, POT2, POT3;
 int value;
 
-
-
-
-#define maxChorusSpeed 30
-#define maxChorusDepth 80
-unsigned int selectedChorusSpeed = 0;
-unsigned int selectedChorusDepth = 0;
-
-#define MAX_DELAY 80
-#define chorusOffset 2
-int DELAY[MAX_DELAY + chorusOffset];
-int Delay2 = 0;
-float delay_sr = 0;
-int delay_int = 0;
-float frac = 0;
-int j = 0;
-
 void setup()
 {
 	my_setup();
@@ -61,38 +44,12 @@ void loop()
 
 
 
-
-int processFlanger(int value)
-{
-	POT0 = map(POT0 >> 2, 0, 1024, 1, 45); //empirical adjusts
-	Delay2 = POT0 / 2;
-
-	for (int u = 0; u <= POT0; u++)
-		DELAY[POT0 + 1 - u] = DELAY[POT0 - u];
-
-	DELAY[0] = value;
-
-	POT1 = map(POT1 >> 2, 0, 1024, 1, 16); // empirical adjusts
-
-	delay_sr = Delay2 - Delay2 * sinf_8k[(j * POT1) / 20];
-	delay_int = int(delay_sr);
-	frac = delay_sr - delay_int;
-
-	frac = betweenF(frac, 0.01, 0.99);
-
-	j++;
-	if (j * POT1 / 20 >= sine_samples)
-		j = 0;
-
-	return (DELAY[delay_int + chorusOffset] * frac + DELAY[delay_int] * (1 - frac));
-}
-
 void TC4_Handler()
 {
 
 	updateValues();
 
-	//value = processFlanger(value);
+	value = processChorus(value);
 	value = processFreqShift(value, selectedFreqShift);
 	value = processDelay(value, selectedDelay);
 
