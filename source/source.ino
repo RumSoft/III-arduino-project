7f1574b6aff2b82c82b6c92b9844b6ca63bacc9e
Dominik Ruman <dr124@users.noreply.github.com>
2019-02-11 15:08:49 +0100

    Update source.ino

--- source/source.ino
+++ source/source.ino
@@ -27,23 +27,27 @@ void loop()
 	while ((ADC->ADC_ISR & 0x3CC0) != 0x3CC0)
 		; // read & wait for ADC
 
-	//read ADC
+	//read ADC audio
 	in_ADC0 = ADC->ADC_CDR[7];
 	in_ADC1 = ADC->ADC_CDR[6];
 
-	//pots ADC8,9,10,11
+	//pots ADC8, 9, 10, 11
 	POT0 = ADC->ADC_CDR[10];
 	POT1 = ADC->ADC_CDR[11];
 	POT2 = ADC->ADC_CDR[12];
 	POT3 = ADC->ADC_CDR[13];
 
+	// CHORUS START
+	// this should be done in TC4_Handler(), but it causes problems there
+	// so I moved chorus part to be before any other processing
 	POT0 = map(POT0 >> 2, 0, 1024, 1, MAX_DELAY); //depth
 	POT1 = map(POT1 >> 2, 0, 1024, 1, 30); //speed
 
 	Delay2 = POT0 / 2;
 	for (int u = 0; u <= POT0; u++)
 		DELAY[POT0 + 1 - u] = DELAY[POT0 - u];
-	DELAY[0] = in_ADC0 - in_ADC1;
+	
+	DELAY[0] = in_ADC0 - in_ADC1;  // value
 
 	delay_sr = Delay2 - Delay2 * sinf_8k[j * POT1/15];
 	delay_int = int(delay_sr);
@@ -54,12 +58,14 @@ void loop()
 		j = 0;
 
 	value = (DELAY[delay_int + 2] * frac + DELAY[delay_int] * (1 - frac));
+	//CHORUS END
 }
 
 void updateValues()
 {
 	TC_GetStatus(TC1, 1); //DAC
 
+	//   commented because chorus is set to top
 	//   value = in_ADC0 - in_ADC1;
 
 	selectedDelay = map(POT3 >> 5, 0, 128, 0, maxDelay);
@@ -72,7 +78,8 @@ void TC4_Handler()
 	updateValues();
 
 	value = processFreqShift(value, selectedFreqShift);
+	//value = processChorus(.... ) //moved to top
 	value = processDelay(value, selectedDelay);
-
+	
 	writeOutput(value);
 }
